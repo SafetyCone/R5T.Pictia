7ae27350bb396f20ec1dd46a055bdbdb284c7e55
MinexAutomation <minex.automation@gmail.com>
2019-10-14 12:56:44 -0700

    Initial commit of source-stub.

---
+++ source/R5T.Pictia.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Pictia.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/R5T.Pictia/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Pictia
+{
+    public class Class1
+    {
+    }
+}
