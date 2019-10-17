7a07b282e90b46943e4d4a186c326181a5e3f050
MinexAutomation <minex.automation@gmail.com>
2019-10-16 17:25:50 -0700

    Added SFTP client wrapper provider interface.

---
+++ source/R5T.Pictia/Code/Interfaces/ISftpClientWrapperProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.Pictia
+{
+    /// <summary>
+    /// Provides an <see cref="SftpClientWrapper"/>, internally performing all necessary work using all necessary dependencies.
+    /// </summary>
+    public interface ISftpClientWrapperProvider
+    {
+        SftpClientWrapper Provide();
+    }
+}
