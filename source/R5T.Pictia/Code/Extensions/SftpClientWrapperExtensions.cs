58e5a539d413174579af33117607cc3efc8e4535
MinexAutomation <minex.automation@gmail.com>
2019-10-21 12:26:31 -0700

    Added extension method to get a connected SSH client from an SFTP client.

---
+++ source/R5T.Pictia/Code/Extensions/SftpClientWrapperExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace R5T.Pictia.Extensions
+{
+    public static class SftpClientWrapperExtensions
+    {
+        public static SshClientWrapper GetSshClientWrapperUnconnected(this SftpClientWrapper sftpClientWrapper)
+        {
+            var sshClientWrapper = new SshClientWrapper(sftpClientWrapper.SftpClient.ConnectionInfo);
+            return sshClientWrapper;
+        }
+
+        public static SshClientWrapper GetSshClientWrapperConnected(this SftpClientWrapper sftpClientWrapper)
+        {
+            var sshClientWrapper = sftpClientWrapper.GetSshClientWrapperUnconnected();
+
+            sshClientWrapper.SshClient.Connect();
+
+            return sshClientWrapper;
+        }
+
+        /// <summary>
+        /// Returns a connected SSH client.
+        /// Uses <see cref="SftpClientWrapperExtensions.GetSshClientWrapperConnected(SftpClientWrapper)"/>.
+        /// </summary>
+        public static SshClientWrapper GetSshClientWrapper(this SftpClientWrapper sftpClientWrapper)
+        {
+            var sshClientWrapper = sftpClientWrapper.GetSshClientWrapperConnected();
+            return sshClientWrapper;
+        }
+    }
+}
