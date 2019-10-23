222bb56c536a35413a4d5264341e0ef6477458ac
MinexAutomation <minex.automation@gmail.com>
2019-10-23 16:04:00 -0700

    Added an SftpClient extension to only list sane (only non-hidden, and not including current directory and parent directory) directory entries.

---
+++ source/R5T.Pictia/Code/Extensions/SftpClientExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Renci.SshNet;
+using Renci.SshNet.Sftp;
+
+using R5T.Magyar.IO;
+
+
+namespace R5T.Pictia.Extensions
+{
+    public static class SftpClientExtensions
+    {
+        public static IEnumerable<SftpFile> ListDirectoryEntriesOnly(this SftpClient sftpClient, string directoryPath, bool showHidden = false)
+        {
+            var allSftpFiles = sftpClient.ListDirectory(directoryPath);
+            foreach (var sftpFile in allSftpFiles)
+            {
+                // If the entry is a directory, and it's one of the relative directory names (i.e. "." or ".."), don't return it.
+                if(sftpFile.IsDirectory)
+                {
+                    var isRelativeDirectoryName = DirectoryNameHelper.IsRelativeDirectoryName(sftpFile.Name);
+                    if(isRelativeDirectoryName)
+                    {
+                        continue;
+                    }
+                }
+
+                // If the entry is hidden-indicated, and we are not showing hidden entries, don't return it.
+                if(PathPartNameHelper.IsHiddenIndicated(sftpFile.Name) && !showHidden)
+                {
+                    continue;
+                }
+
+                // After all exceptions, return the entry.
+                yield return sftpFile; // OK, just return!
+            }
+        }
+    }
+}
